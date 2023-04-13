fe01f1cfc64dbd26f3ead3d1235751e055f9ddb5
ayon_ssp <ayon.ssp@gmail.com>
2023-04-13 15:02:58 +0530

    Post Request added

--- django_rest/api/urls.py
+++ django_rest/api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getData),
+    path('add/', views.addItem),
+
 ]
--- django_rest/api/views.py
+++ django_rest/api/views.py
@@ -22,3 +22,12 @@ def getData(request):
     items = Item.objects.all()
     serializer = ItemSerializer(items, many=True)
     return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+def addItem(request):
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
